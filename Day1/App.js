bfbf42a52bdb2b7858b29ad4656925055463c9dc
utkrista <kcutkrista92@gmail.com>
2023-01-07 11:16:41 +1100

    Day 1

---
+++ Day1/App.js
@@ -0,0 +1,9 @@
+//Passing multiple childs to  div container
+const heading = React.createElement("h1", { id: "title" }, "Heading 1");
+const heading2 = React.createElement("h2", { id: "title" }, "Heading 2");
+const container = React.createElement("div", { id: "container" }, [
+  heading,
+  heading2,
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
